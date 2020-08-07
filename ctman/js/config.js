c525b152f8e0a445d3bd0edf4f36fa62a3f42da5
bubbleboy14 <mario.balibrera@gmail.com>
2020-08-07 00:47:38 -0700

    config.template[] w/ description{}

--- ctman/js/config.js
+++ ctman/js/config.js
@@ -1,4 +1,9 @@
 {
+	"template": [{
+		"isTA": true,
+		"name": "description",
+		"blurs": ["please describe", "please expand", "tell all here"]
+	}],
     "injections": [{
         "name": "manager",
         "blurs": ["manager's name", "who is the manager?"]
