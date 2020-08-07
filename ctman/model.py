c59a474d876b479cf1e8b4d8f501a84ea96606df
bubbleboy14 <mario.balibrera@gmail.com>
2020-08-07 11:36:08 -0700

    SecBase: secs() returns concatonated subsections; content() checks for sections[] before calling secs() and log()s content

--- ctman/model.py
+++ ctman/model.py
@@ -1,4 +1,5 @@
 from cantools import db
+from cantools.util import log
 from ctuser.model import *
 from ctedit.model import PageEdit, Style
 
@@ -8,13 +9,16 @@ class SecBase(db.TimeStampedBase):
 	sections = db.ForeignKey(kind="section", repeated=True)
 
 	def secs(self, sections=None, depth=1):
-		"\r\n\r\n".join(sections and [
+		return "\r\n\r\n".join(sections and [
 			db.get(s['key']).content(s['sections'], depth) for s in sections
 		] or [s.content(depth=depth) for s in db.get_multi(self.sections)])
 
 	def content(self, sections=None, depth=1):
-		return "\r\n\r\n".join(["%s %s"%("#" * depth, self.name),
-			self.description, self.secs(sections, depth + 1)])
+		tline = "%s %s"%("#" * depth, self.name)
+		secs = self.sections and self.secs(sections, depth + 1) or ""
+		cont = "\r\n\r\n".join([tline, self.description, secs])
+		log(cont)
+		return cont
 
 	def unrolled(self):
 		d = self.data()
