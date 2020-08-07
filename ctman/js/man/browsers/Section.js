540fe3514c84c5ae06e772081d8d84e91325f00a
bubbleboy14 <mario.balibrera@gmail.com>
2020-08-07 03:58:35 -0700

    browsers: switched Template/Section inheritance (Template now inherits from Section)

--- ctman/js/man/browsers/Section.js
+++ ctman/js/man/browsers/Section.js
@@ -1,5 +1,23 @@
 man.browsers.Section = CT.Class({
 	CLASSNAME: "man.browsers.Section",
+	view: function(d) {
+		var _ = this._, mcfg = core.config.ctman, val;
+		CT.dom.setContent(_.nodes.content, [
+			CT.dom.div(d.name, "bigger centered"),
+			man.util.form(d, "template", function(vals) {
+				for (val in vals)
+					d[val] = vals[val];
+				_.edit(d.key ? CT.merge({
+					key: d.key
+				}, vals) : d);
+			})
+		]);
+	},
+	defaults: function() {
+		return {
+			sections: []
+		};
+	},
 	init: function(opts) {
 		this.opts = CT.merge(opts, {
 			owner: false,
@@ -7,4 +25,4 @@ man.browsers.Section = CT.Class({
 			blurs: ["section name", "section title", "name that section"]
 		}, this.opts);
 	}
-}, man.browsers.Template);
+}, CT.Browser);
--- ctman/js/man/browsers/Template.js
+++ ctman/js/man/browsers/Template.js
@@ -1,27 +1,10 @@
 man.browsers.Template = CT.Class({
 	CLASSNAME: "man.browsers.Template",
-	view: function(d) {
-		var _ = this._, mcfg = core.config.ctman, val;
-		CT.dom.setContent(_.nodes.content, [
-			CT.dom.div(d.name, "bigger centered"),
-			man.util.form(d, "template", function(vals) {
-				for (val in vals)
-					d[val] = vals[val];
-				_.edit(d.key ? CT.merge({
-					key: d.key
-				}, vals) : d);
-			})
-		]);
-	},
-	defaults: function() {
-		return {
-			sections: []
-		};
-	},
-	init: function(opts) {
+	init: function(opts) { // requires opts.sections
 		this.opts = CT.merge(opts, {
+			owner: true,
 			modelName: "template",
 			blurs: ["template name", "template title", "name that template"]
 		}, this.opts);
 	}
-}, CT.Browser);
+}, man.browsers.Section);
