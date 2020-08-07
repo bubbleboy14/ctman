2ad6ff0040258ea15a9eeff3c55d9e0ab66ab4bb
bubbleboy14 <mario.balibrera@gmail.com>
2020-08-07 04:33:28 -0700

    templates page loader: man.browsers.Section import; split screen; load Section browser in bottom half and Template browser (with Section browser reference) in top half

--- ctman/js/man/pages/templates.js
+++ ctman/js/man/pages/templates.js
@@ -2,9 +2,19 @@ CT.require("CT.all");
 CT.require("core");
 CT.require("user.core");
 CT.require("man.util");
+CT.require("man.browsers.Section");
 CT.require("man.browsers.Template");
 
 CT.onload(function() {
 	CT.initCore();
-	new man.browsers.Template();
+	var tophalf = CT.dom.div(null, "abs tophalf"),
+		bottomhalf = CT.dom.div(null, "abs bottomhalf");
+	CT.dom.setContent("ctmain", [tophalf, bottomhalf]);
+	var secs = new man.browsers.Section({
+		parent: bottomhalf
+	});
+	new man.browsers.Template({
+		parent: tophalf,
+		sections: secs
+	});
 });
