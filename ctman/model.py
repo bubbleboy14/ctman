3e71e94ecd3015dc42ae74c3c6cb067e681e4c2b
bubbleboy14 <mario.balibrera@gmail.com>
2020-08-06 21:34:01 -0700

    model: Seciton table, with name, description, and secitons[]; Template table, which inherits from Section; Document.template

--- ctman/model.py
+++ ctman/model.py
@@ -2,8 +2,17 @@ from cantools import db
 from ctuser.model import *
 from ctedit.model import PageEdit, Style
 
+class Section(db.TimeStampedBase):
+	name = db.String()
+	description = db.Text()
+	sections = db.ForeignKey(kind="section", repeated=True)
+
+class Template(Section):
+	pass
+
 class Document(db.TimeStampedBase):
 	owner = db.ForeignKey(kind=CTUser)
+	template = db.ForeignKey(kind=Template)
 	name = db.String()
 	injections = db.JSON()
 	assembly = db.JSON()
