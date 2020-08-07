82fcaa0a58e4fc70b0625fff1958a58705fa96e8
bubbleboy14 <mario.balibrera@gmail.com>
2020-08-07 04:56:11 -0700

    Section browser sections[] management updates (also applies to Template subclass): asec(), which returns a section selection function; section(), which returns a section .choice_cell that, when clicked, reveals corresponding section entry (in Section browser [bottom half of page]); sections(), which returns a node with refresh()able contents including an asec()-derived "add section" button and a section()-mapped sections[] listing; view() adds sections() node to any initialized (containing a key) section

--- ctman/js/man/browsers/Section.js
+++ ctman/js/man/browsers/Section.js
@@ -1,5 +1,44 @@
 man.browsers.Section = CT.Class({
 	CLASSNAME: "man.browsers.Section",
+	asec: function(d, n) {
+		var sbro = this.opts.sections || this,
+			nonoz = [d.key].concat(d.sections);
+		return function() {
+			items = sbro._.items.filter(i => !nonoz.includes(i.key));
+			items.length ? CT.modal.choice({
+				prompt: "please select a section",
+				data: items,
+				cb: function(sec) {
+					d.sections.push(sec.key);
+					CT.db.put({
+						key: d.key,
+						sections: d.sections
+					}, n.refresh);
+				}
+			}) : alert('click "new section" to create a new section');
+		};
+	},
+	section: function(key) {
+		var s = CT.data.get(key);
+		return CT.dom.div(s.name, "choice_cell", null, {
+			onclick: function() {
+				CT.dom.id("tl" + key).trigger();
+			}
+		});
+	},
+	sections: function(d) {
+		var n = CT.dom.div(null, "margined padded bordered round"),
+			section = this.section, asec = this.asec;
+		n.refresh = function() {
+			CT.dom.setContent(n, [
+				CT.dom.button("add section", asec(d, n), "right"),
+				"sections",
+				d.sections.map(section)
+			]);
+		};
+		n.refresh();
+		return n;
+	},
 	view: function(d) {
 		var _ = this._, mcfg = core.config.ctman, val;
 		CT.dom.setContent(_.nodes.content, [
@@ -10,7 +49,8 @@ man.browsers.Section = CT.Class({
 				_.edit(d.key ? CT.merge({
 					key: d.key
 				}, vals) : d);
-			})
+			}),
+			d.key && this.sections(d)
 		]);
 	},
 	defaults: function() {
