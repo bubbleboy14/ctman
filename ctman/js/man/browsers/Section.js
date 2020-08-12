48fcd98a9add90004fc3ce8fc6429b409f510127
bubbleboy14 <mario.balibrera@gmail.com>
2020-08-12 01:18:49 -0700

    Section browser (also applies to Template browser): move(), which offers choice() prompt with up (if not top), remove, and down (if not bottom) options - on selection, shift/remove node, update d.sections[], and push changes to server; section() accepts d argument (section/template data object) and includes move button (which triggers move()) in returned node cluster; sections() passes d{} to this.section()

--- ctman/js/man/browsers/Section.js
+++ ctman/js/man/browsers/Section.js
@@ -19,17 +19,55 @@ man.browsers.Section = CT.Class({
 			}) : alert('click "new section" to create a new section');
 		};
 	},
-	section: function(key) {
-		var s = CT.data.get(key);
-		return CT.dom.div(s.name, "choice_cell", null, {
-			onclick: function() {
-				CT.dom.id("tl" + key).trigger();
+	move: function(key, d, shnode) {
+		var options = [], freshies = this.refreshers;
+		if (key != d.sections[0])
+			options.push("up");
+		options.push("remove");
+		if (key != d.sections[d.sections.length - 1])
+			options.push("down");
+		CT.modal.choice({
+			prompt: "shift or remove?",
+			data: options,
+			cb: function(move) {
+				var i = CT.dom.childNum(shnode),
+					el = d.sections.splice(i, 1)[0];
+				if (move == "remove")
+					shnode.remove();
+				else if (move == "up") {
+					d.sections.splice(i - 1, 0, el);
+					shnode.parentNode.insertBefore(shnode, shnode.previousSibling);
+				} else {
+					d.sections.splice(i + 1, 0, el);
+					if (shnode.nextSibling.nextSibling)
+						shnode.parentNode.insertBefore(shnode, shnode.nextSibling.nextSibling);
+					else
+						shnode.parentNode.appendChild(shnode);
+				}
+				CT.db.put({
+					key: d.key,
+					sections: d.sections
+				});
 			}
 		});
 	},
+	section: function(key, d) {
+		var s = CT.data.get(key), n = CT.dom.div([
+			CT.dom.button("move", _ => this.move(key, d, n), "right"),
+			s.name
+		], "choice_cell", null, {
+			onclick: function() {
+				CT.dom.id("tl" + key).trigger();
+			}
+		})
+		return n;
+	},
 	sections: function(d) {
+		var sec = this.section;
 		return man.util.refresher("sections", "add section",
-			n => this.asec(d, n), _ => d.sections.map(this.section));
+			n => this.asec(d, n), function() {
+				return d.sections.map(s => sec(s, d));
+			});
 	},
 	view: function(d) {
 		var _ = this._, mcfg = core.config.ctman, val;
