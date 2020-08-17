72763a4208735532efa1b2a2fa43f987b921995d
bubbleboy14 <mario.balibrera@gmail.com>
2020-08-17 02:16:12 -0700

    Template browser, constructor, default opts: headerlessness false

--- ctman/js/man/browsers/Template.js
+++ ctman/js/man/browsers/Template.js
@@ -6,6 +6,7 @@ man.browsers.Template = CT.Class({
 	init: function(opts) { // requires opts.sections
 		this.opts = CT.merge(opts, {
 			owner: true,
+			headerlessness: false,
 			modelName: "template",
 			blurs: ["template name", "template title", "name that template"]
 		}, this.opts);
