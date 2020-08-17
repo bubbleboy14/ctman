2903f3d375aa0b8d3178b8b0af7b4fed5cbd142f
bubbleboy14 <mario.balibrera@gmail.com>
2020-08-17 03:11:31 -0700

    Templates page loader: include man.util.sideslide() toggle

--- ctman/js/man/pages/templates.js
+++ ctman/js/man/pages/templates.js
@@ -9,7 +9,11 @@ CT.onload(function() {
 	CT.initCore();
 	var tophalf = CT.dom.div(null, "abs tophalf"),
 		bottomhalf = CT.dom.div(null, "abs bottomhalf");
-	CT.dom.setContent("ctmain", [tophalf, bottomhalf]);
+	CT.dom.setContent("ctmain", [
+		tophalf,
+		bottomhalf,
+		man.util.sideslide()
+	]);
 	var secs = new man.browsers.Section({
 		parent: bottomhalf
 	});
