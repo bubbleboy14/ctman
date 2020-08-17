a378e4f3ede7cd86a0784bffc35e20b3f5ebb743
bubbleboy14 <mario.balibrera@gmail.com>
2020-08-17 01:38:42 -0700

    builder.inject(): {{double}} curlies

--- ctman/builder.py
+++ ctman/builder.py
@@ -20,7 +20,7 @@ def hazard(template, arules): # do non-chems as well
 
 def inject(data, injects):
 	for i in injects:
-		data = data.replace("{%s}"%(i,), injects[i])
+		data = data.replace("{{%s}}"%(i,), injects[i])
 	return data
 
 SUSHEET = """\\begin{center}
