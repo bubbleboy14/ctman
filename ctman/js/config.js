d704111cd5b2f4f165628d1f1bb975cd0dd34ace
bubbleboy14 <mario.balibrera@gmail.com>
2020-08-18 20:07:54 -0700

    cfg, template[]: description wyz true

--- ctman/js/config.js
+++ ctman/js/config.js
@@ -1,21 +1,22 @@
 {
 	"template": [{
+		"wyz": true,
 		"isTA": true,
 		"name": "description",
 		"blurs": ["please describe", "please expand", "tell all here"]
 	}],
-    "injections": [{
-        "name": "manager",
-        "blurs": ["manager's name", "who is the manager?"]
-    }, {
-        "name": "location",
-        "blurs": ["where is it?", "location"]
-    }, {
-        "isTA": true,
-        "name": "description",
-        "blurs": ["what's the big idea?", "please describe", "tell me more"]
-    }],
-    "hazards": {
-        "chemical": ["Arsenic", "Lead", "4,4 DDE"]
-    }
+	"injections": [{
+		"name": "manager",
+		"blurs": ["manager's name", "who is the manager?"]
+	}, {
+		"name": "location",
+		"blurs": ["where is it?", "location"]
+	}, {
+		"isTA": true,
+		"name": "description",
+		"blurs": ["what's the big idea?", "please describe", "tell me more"]
+	}],
+	"hazards": {
+		"chemical": ["Arsenic", "Lead", "4,4 DDE"]
+	}
 }
