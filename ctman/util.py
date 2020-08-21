860cd31bca743c31040904d5f6cd8f79128d4223
bubbleboy14 <mario.balibrera@gmail.com>
2020-08-20 20:21:33 -0700

    Util module, HTML->LaTeX snippet conversion, bold and italics: flags{} rules object; h2l() translation function, which loops through flags (and corresponding rulesets), latexizes translatable snippets (of a given HTML segment), and returns the transformed string

--- ctman/util.py
+++ ctman/util.py
@@ -1,16 +1,31 @@
-#<p><span style="color: #ff0000;">this is it!</span></p>
+flags = {
+	"color:": {
+		"start": '<span style="color: #',
+		"end": "</span>",
+		"mid": ';">',
+		"tex": "\\textcolor[HTML]{%s}{%s}"
+	},
+	"strong": {
+		"tex": "\\textbf{%s}"
+	},
+	"em": {
+		"tex": "\\emph{%s}"
+	}
+}
 
-sflag = '<span style="color: #'
-slen = len(sflag)
-eflag = "</span>"
-elen = len(eflag)
-mflag = ';">'
-ltmp = "\\textcolor[HTML]{%s}{%s}"
-
-def colfix(h):
-	while sflag in h:
-		start = h.index(sflag)
-		end = h.index(eflag)
-		[c, t] = h[start + slen:end].split(mflag)
-		h = h[:start] + ltmp%(c, t) + h[end + elen:]
+def h2l(h):
+	for flag in flags:
+		rules = flags[flag]
+		sflag = rules.get("start", "<%s>"%(flag,))
+		eflag = rules.get("end", "</%s>"%(flag,))
+		tex = rules["tex"]
+		while sflag in h:
+			start = h.index(sflag)
+			end = h.index(eflag)
+			seg = h[start + len(sflag):end]
+			if "mid" in rules:
+				[c, t] = seg.split(rules["mid"])
+				h = h[:start] + tex%(c, t) + h[end + len(eflag):]
+			else:
+				h = h[:start] + tex%(seg,) + h[end + len(eflag):]
 	return h
