e1bbabbd5fbb9e84583e4cca02655795a61aa607
bubbleboy14 <mario.balibrera@gmail.com>
2020-09-16 13:42:57 -0700

    Template browser: choosevar() alert()s (instead of choice() modal prompting) if !template.injections.length; goodsecs() injection variable scanner / sections[] update validation function, which (largely via injections.extract()) determines whether unregistered injection variables are present in candidate sections (if yes, alert()s - else, approves sections[] update)

--- ctman/js/man/browsers/Template.js
+++ ctman/js/man/browsers/Template.js
@@ -1,11 +1,18 @@
 man.browsers.Template = CT.Class({
 	CLASSNAME: "man.browsers.Template",
+	goodsecs: function(d, secs) {
+		var newsecs = secs.filter(s => !d.sections.includes(s.key)),
+			sinjs = man.injections.extract(newsecs),
+			tinjs = CT.data.getSet(d.injections).map(i => i.name),
+			bads = sinjs.filter(i => !tinjs.includes(i));
+		return bads.length ? alert("For these Sections to render properly you will need to define injection variables for the following: " + bads.join(", ")) : true;
+	},
 	choosevar: function(d, cb) {
-		CT.modal.choice({
+		d.injections.length ? CT.modal.choice({
 			prompt: "choose an injection variable",
 			data: d.injections.map(i => CT.data.get(i).name),
 			cb: cb
-		});
+		}) : alert("no injection variables defined! please define some :)");
 	},
 	extra: function(d) {
 		return d.key && [
