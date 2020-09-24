dcf3745471e538c35df9025afbfd784b28179bde
bubbleboy14 <mario.balibrera@gmail.com>
2020-09-24 03:42:15 -0700

    latline() \n-wraps output

--- ctman/util.py
+++ ctman/util.py
@@ -218,7 +218,7 @@ def latline(line):
 	for i in range(6):
 		flag = hflags[i]
 		if line.startswith(flag):
-			return "%s{%s}\\normalsize"%(lahead[i], line[len(flag):])
+			return "\n%s{%s}\\normalsize\n"%(lahead[i], line[len(flag):])
 	return line
 
 def lhead(h):
