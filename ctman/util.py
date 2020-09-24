014833de2f92ae8758dbe349d4a05d42484f9d1e
bubbleboy14 <mario.balibrera@gmail.com>
2020-09-24 00:43:22 -0700

    latline() uses curlies ({}) instead of prepended \normalsize (which messes up multi-line <H[n]> node translation)

--- ctman/util.py
+++ ctman/util.py
@@ -206,7 +206,7 @@ def latline(line):
 	for i in range(6):
 		flag = hflags[i]
 		if line.startswith(flag):
-			return "%s %s\n\\normalsize\n"%(lahead[i], line[len(flag):])
+			return "%s{%s}"%(lahead[i], line[len(flag):])
 	return line
 
 def lhead(h):
