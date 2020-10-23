f45649e41b62655ae0aea2f390c410912514ebeb
bubbleboy14 <mario.balibrera@gmail.com>
2020-10-22 18:42:52 -0700

    Trans.html2latex.util: added swaps{} and UTF-8 coding flag

--- ctman/trans/html2latex/util.py
+++ ctman/trans/html2latex/util.py
@@ -1,5 +1,25 @@
+# -*- coding: UTF-8 -*-
+
 headers = {
 	"latex": [ "\\large", "\\large", "\\Large", "\\LARGE", "\\huge", "\\Huge" ],
 	"section": ["%s "%("#" * i,) for i in range(1, 7)]
 }
 headers["section"].reverse()
+
+# maybe rm some swaps???
+swaps = {
+	"_": "\\_",
+	"<p>|": "|",
+	"|</p>": "|",
+	"&sect;": "§",
+	"&ndash;": "-",
+	"<br />": " \\hfill\\break ",
+	"&amp;": "\\&",
+	"&mu;": "$\\mu$",
+	"&ldquo;": '"',
+	"&rdquo;": '"',
+	"&bull;": "\\textbullet",
+	"text-align: left; ": "",
+	"padding-left: 60px; text-align: center;": "text-align: center;",
+	'<span style="text-align: center; ': '<span style="'
+}
