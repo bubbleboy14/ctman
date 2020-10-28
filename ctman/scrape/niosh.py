d350f8874dcc09e855a7d4040e27e67137a9817e
bubbleboy14 <mario.balibrera@gmail.com>
2020-10-27 20:03:38 -0700

    Coalesced scrape.niosh.Scraper class: slightly tweaked/adapted acquire() and index(); chems() index page array processor (skeletal); download(), which acquires/builds datasets (via index() and [emptyish] chems()); chem() (emptyish) and process(), which calls chem() (processor) on each chemical page; constructor (somewhat derived from original scrape() function), which (if necessary) builds directory hierarchy, establishes pages{index[],chems[]}, and calls download() followed by process()

--- ctman/scrape/niosh.py
+++ ctman/scrape/niosh.py
@@ -1,6 +1,6 @@
 import os, string
 from cantools.web import fetch
-from cantools.util import mkdir, write
+from cantools.util import log, mkdir, read, write
 
 IURL = "https://www.cdc.gov/niosh/npg/npgsyn-%s.html"
 NDIR = os.path.join("scrape", "niosh")
@@ -8,19 +8,42 @@ FULL = os.path.join(NDIR, "full")
 INDEX = os.path.join(FULL, "index")
 CHEMS = os.path.join(FULL, "chems")
 
-def acquire(url, path):
-	fname = url.split("/").pop()
-	fpath = os.path.join(path, fname)
-	if not os.path.exists(fpath):
+class Scraper(object):
+	def __init__(self):
+		for p in [NDIR, FULL, INDEX, CHEMS]:
+			if not os.path.isdir(p):
+				mkdir(p)
+		self.pages = {
+			"index": [],
+			"chems": []
+		}
+		self.download()
+		self.process()
+
+	def acquire(self, url, path):
+		fname = url.split("/").pop()
+		fpath = os.path.join(path, fname)
+		if os.path.exists(fpath):
+			return read(fpath)
 		data = fetch(url)
 		write(data, fpath)
+		return data
 
-def index():
-	for letter in string.lowercase:
-		acquire(IURL%(letter,), INDEX)
+	def index(self):
+		for letter in string.lowercase:
+			self.pages["index"].append(self.acquire(IURL%(letter,), INDEX))
 
-def scrape():
-	for p in [NDIR, FULL, INDEX, CHEMS]:
-		if not os.path.isdir(p):
-			mkdir(p)
-	index()
+	def chems(self):
+		for page in self.pages["index"]:
+			pass # derive chem pages from index pages
+
+	def download(self):
+		self.index()
+		self.chems()
+
+	def chem(self, page):
+		pass # process chem page
+
+	def process(self):
+		for page in self.pages["chems"]:
+			self.chem(page)
