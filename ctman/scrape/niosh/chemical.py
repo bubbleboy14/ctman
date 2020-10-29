663d84ec1f0f4edebcab8c368d6ccced2a0c7fe6
bubbleboy14 <mario.balibrera@gmail.com>
2020-10-28 17:34:36 -0700

    Created new scraper.niosh submodule, chemical. Added data structures (to scrape.niosh.chemical) defining the rules for extracting various data from chemical information page: EXTRACTS (basic extraction) - name; CARDS (card-style properties with custom flags) - cas, rtecs, dot, idlh; TCARDS (title-style cards) - 19 items; HCARDS (with arbitrary HTML we want to grab - extract() with unquote False) - exposure_limits, measurement_methods, first_aid. New class (in scrape.niosh.chemical), Chem: extract(), which grabs and returns a bit of text (based on f (from [start] string), start (pre-start index - default None), t (to [end] string - default </div>), and unquote bool (default True)) from a chemical information page; card(), which grabs (via extract()) and returns a bit of text in the standard niosh card format; tcard() (for title cards), which transforms a column name (such as synonyms_and_trade_names) into a title flag (such as Synonyms & Trade Names), and uses card() to extract and return the corresponding value; scrape(), which extracts EXTRACTS (via extract()), CARDS (via card()), TCARDS (via tcard()), and HCARDS (via tcard() with unquote False).

---
+++ ctman/scrape/niosh/chemical.py
@@ -0,0 +1,50 @@
+EXTRACTS = {
+	"name": 'mobile-title">'
+}
+CARDS = {
+	"cas": "CAS No."
+	"rtecs": "RTECS No.",
+	"dot": "DOT ID & Guide",
+	"idlh": "IDLH"
+}
+TCARDS = ["synonyms_and_trade_names", "formula", "conversion",
+	"physical_description", "molecular_weight", "boiling_point",
+	"freezing_point", "solubility", "vapor_pressure",
+	"ionization_potential", "specific_gravity", "flash_point",
+	"upper_explosive_limit", "lower_explosive_limit",
+	"incompatibilities_and_reactivities",
+	"exposure_routes", "symptoms", "target_organs",
+	"respirator_recommendations"]
+HCARDS = ["exposure_limits", "measurement_methods", "first_aid"]
+# needs: Personal Protection/Sanitation, Classification
+
+class Chem(object):
+	def __init__(self, page):
+		self.page = page
+		self.data = {}
+		self.scrape()
+
+	def extract(self, f, start=None, t="</div>", unquote=True):
+		s = self.page.index(f, start) + len(f)
+		bit = self.page[s : self.page.index(t, s)]
+		if unquote and '"' in bit:
+			bit = bit.split('"')[1]
+		return bit.strip()
+
+	def card(self, flag, unquote=True):
+		return self.extract('card-text">',
+			self.page.index(flag), unquote=unquote)
+
+	def tcard(self, flag, unquote=True):
+		flag = flag.replace("_and_", " & ").replace("_", " ")
+		return self.card(flag.title(), unquote=unquote)
+
+	def scrape(self):
+		for name, flag in EXTRACTS.items():
+			self.data[name] = self.extract(flag)
+		for name, flag in CARDS.items():
+			self.data[name] = self.card(flag)
+		for name in TCARDS:
+			self.data[name] = self.tcard(name)
+		for name in HCARDS:
+			self.data[name] = self.tcard(name, False)
