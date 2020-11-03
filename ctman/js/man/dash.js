c9d4eb4aa7a1ad1b4e856ba2f255719e5c7f9075
bubbleboy14 <mario.balibrera@gmail.com>
2020-11-03 00:15:10 -0800

    Dash module: build() includes created/modified columns

--- ctman/js/man/dash.js
+++ ctman/js/man/dash.js
@@ -1,13 +1,14 @@
 man.dash = {
 	build: function(dox) {
 		CT.dom.setContent("ctmain", CT.dom.flex([
-			CT.dom.flex(["name", "template", "revision"],
+			CT.dom.flex(["name", "template", "revision", "created", "modified"],
 				"row jccenter big bold")
 		].concat(dox.map(d => CT.dom.flex([
 			CT.dom.link(d.name, null, "/man#" + d.key),
 			d.template ? CT.dom.link(d.template.name, null,
 				"/man/templates.html#" + d.template.key) : "(none)",
-			CT.dom.link(d.revision, null, "/" + d.pdf, null, null, null, true)
+			CT.dom.link(d.revision, null, "/" + d.pdf, null, null, null, true),
+			d.created, d.modified
 		], "row jccenter"))), "col margined"));
 	},
 	init: function() {
