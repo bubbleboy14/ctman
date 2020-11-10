9f83d5c4cd36a609cc5a30e135fcdd1c12089741
bubbleboy14 <mario.balibrera@gmail.com>
2020-11-09 17:26:35 -0800

    Dash module: spliced cfg.ctman.declarations{} (names) into _.cols[]; doc() includes declarations in returned (flex) table row

--- ctman/js/man/dash.js
+++ ctman/js/man/dash.js
@@ -1,18 +1,22 @@
+var DEX = core.config.ctman.declarations.map(v => v.name);
+
 man.dash = {
 	_: {
-		cols: ["name", "template", "revision",
-			"created", "revised", "expires", "delete"]
+		cols: ["name", "template"].concat(DEX).concat(["revision",
+			"created", "revised", "expires", "delete"])
 	},
 	doc: function(d) {
 		var cre = d.created.split(" ").shift(),
 			rev = d.modified.split(" ").shift(),
 			ry = rev.split("-").shift(),
 			ey = (parseInt(ry) + 1).toString(),
-			exp = rev.replace(ry, ey);
-		return CT.dom.flex([
-			CT.dom.link(d.name, null, "/man#" + d.key),
-			d.template ? CT.dom.link(d.template.name, null,
-				"/man/templates.html#" + d.template.key) : "(none)",
+			exp = rev.replace(ry, ey), content = [
+				CT.dom.link(d.name, null, "/man#" + d.key),
+				d.template ? CT.dom.link(d.template.name, null,
+					"/man/templates.html#" + d.template.key) : "(none)"
+			];
+		content = content.concat(DEX.map(v => d.declarations[v]));
+		content = content.concat([
 			CT.dom.link(d.revision, null, "/" + d.pdf, null, null, null, true),
 			cre, rev, exp, CT.dom.link("delete", function() {
 				if (!(confirm("are you sure you want to delete this section?")
@@ -22,7 +26,8 @@ man.dash = {
 					CT.dom.id(d.key).remove();
 				}, "delete", "key");
 			}, null, "red")
-		], "row jccenter", d.key);
+		]);
+		return CT.dom.flex(content, "row jccenter", d.key);
 	},
 	build: function(dox) {
 		CT.dom.setContent("ctmain", CT.dom.flex([
@@ -34,4 +39,4 @@ man.dash = {
 			owner: user.core.get("key")
 		}, null, null, "summary");
 	}
-}
+};
