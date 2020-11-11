f3098461b23f1099eaf1fc00d46fddb6bba1f9c6
bubbleboy14 <mario.balibrera@gmail.com>
2020-11-10 20:45:34 -0800

    Account module: _{}, with nodes{payform,options,info}, refresh(), ifresh(), info(), form(), plan(); init() - CT.pay.init() cb() calls _.play() for each configuration-defined item/amount pairing, _.ifresh(), and add all nodes (info, options, payform) to page

--- ctman/js/man/account.js
+++ ctman/js/man/account.js
@@ -1,40 +1,60 @@
 man.account = {
 	_: {
-		renew: function(expiration, info) {
+		nodes: {
+			payform: CT.dom.div(),
+			options: CT.dom.div(),
+			info: CT.dom.div()
+		},
+		refresh: function(expiration) {
 			user.core.update({
 				expiration: expiration
 			});
-			CT.dom.setContent(info,
-				man.account.info());
+			man.account._.ifresh();
 			alert("success!");
+		},
+		ifresh: function() {
+			CT.dom.setContent(man.account._.nodes.info,
+				man.account._.info());
+		},
+		info: function() { // TODO: payment history etc
+			var u = user.core.get();
+			if (u.admin) return "you're an admin";
+			if (!u.expiration) return "you haven't finished your registration";
+			var n = new Date(), e = new Date(u.expiration),
+				ex = u.expiration.split(" ").shift();
+			return "your account " + ((n > e) ?
+				"expires" : "expired") + " on " + ex;
+		},
+		form: function(item, amount) {
+			var _ = man.account._, pf = _.nodes.payform;
+			CT.dom.clear(pf);
+			new CT.pay.Form({
+				parent: pf,
+				item: "one month",
+				amount: "$10.00",
+				onpaid: _.refresh
+			});
+		},
+		plan: function(item, amount) {
+			var _ = man.account._, butt = CT.dom.button(item + " for " + amount,
+				e => _.form(item, amount));
+			_.nodes.options.appendChild(butt);
 		}
 	},
-	info: function() { // TODO: payment history etc
-		var u = user.core.get();
-		if (u.admin) return "you're an admin";
-		if (!u.expiration) return "you haven't finished your registration";
-		var n = new Date(), e = new Date(u.expiration),
-			ex = u.expiration.split(" ").shift();
-		return "your account " + ((n > e) ?
-			"expires" : "expired") + " on " + ex;
-	},
 	init: function() {
-		var payform = CT.dom.div(), renew = this._.renew,
-			info = CT.dom.div(man.account.info());
+		var _ = man.account._, nz = _.nodes,
+			cfg = core.config.ctman, i;
 		CT.pay.init({
 			mode: "braintree",
 			cb: function() {
-				new CT.pay.Form({
-					parent: payform,
-					item: "one month",
-					amount: "$10.00",
-					onpaid: ex => renew(ex, info)
-				});
+				for (i in cfg.subs)
+					_.plan(i, cfg.subs[i]);
 			}
 		});
+		_.ifresh();
 		CT.dom.setMain([
 			"Hello " + user.core.get("firstName"),
-			info, payform
+			nz.info, nz.options, nz.payform
 		]);
 	}
 };
