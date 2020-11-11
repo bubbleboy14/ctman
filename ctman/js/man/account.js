ebf725ce5879202e8764f63e43fe4b847a8004e7
bubbleboy14 <mario.balibrera@gmail.com>
2020-11-10 19:39:51 -0800

    Account module, init(): added CT.pay.Form params, including item, amount, and onpaid() callback (which updates user expiration (via user.core.update()), refreshes contents of info node, and alert()s user of success)

--- ctman/js/man/account.js
+++ ctman/js/man/account.js
@@ -9,16 +9,29 @@ man.account = {
 			"expires" : "expired") + " on " + ex;
 	},
 	init: function() {
-		var payform = CT.dom.div();
+		var payform = CT.dom.div(),
+			info = CT.dom.div(man.account.info());
 		CT.pay.init({
 			mode: "braintree",
 			cb: function() {
-				new CT.pay.Form({ parent: payform });
+				new CT.pay.Form({
+					parent: payform,
+					item: "one month",
+					amount: "$10.00",
+					onpaid: function(expiration) {
+						user.core.update({
+							expiration: expiration
+						});
+						CT.dom.setContent(info,
+							man.account.info());
+						alert("success!");
+					}
+				});
 			}
 		});
 		CT.dom.setMain([
 			"Hello " + user.core.get("firstName"),
-			man.account.info(), payform
+			info, payform
 		]);
 	}
 };
