e990be86c0e3b2f2d1e6b52272caad7042684c21
bubbleboy14 <mario.balibrera@gmail.com>
2022-09-03 16:26:37 -0700

    new trans.html2docx module, featuring swaps{NEWPAGE:DXPB}, styles{text-align{center/right/left:dxtl()}}, and H2X(Converter)

---
+++ ctman/trans/html2docx.py
@@ -0,0 +1,27 @@
+from .util import Converter
+
+DXPB = """```{=openxml}
+<w:p>
+  <w:r>
+    <w:br w:type="page"/>
+  </w:r>
+</w:p>
+```"""
+
+def dxtl(alignment):
+	return '```{=openxml}\n<w:jc w:val="' + alignment + '">\n  %s\n</w:jc>\n```'
+
+swaps = {
+	"NEWPAGE": DXPB
+}
+styles = {
+	"text-align": {
+		"center": dxtl("center"),
+		"right": dxtl("right"),
+		"left": dxtl("left")
+	}
+}
+
+class H2X(Converter):
+	def __init__(self, fragment, depth=0, swappers=swaps, flaggers={}, styles=styles, cstyles={}, loud=True):
+		Converter.__init__(self, fragment, depth, swappers, flaggers, styles, cstyles, loud)
