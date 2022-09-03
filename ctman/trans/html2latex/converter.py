4333079dacdd662ea0fbcc0f79d67ffb4a1ab7fb
bubbleboy14 <mario.balibrera@gmail.com>
2022-09-03 16:00:21 -0700

    html2latex.converter - converted H2L to Converter subclass

--- ctman/trans/html2latex/converter.py
+++ ctman/trans/html2latex/converter.py
@@ -1,44 +1,18 @@
-from cantools import config
-from .util import TABLE_FLAGS, flags, swaps, trans, nextlast
+from .util import TABLE_FLAGS, flags, swaps, styles, cstyles, trans, Converter
 from .header import Header
-from .fragment import Fragment
 
-class H2L(object):
-	def __init__(self, fragment, depth):
-		self.fragment = fragment
-		self.depth = depth
+class H2L(Converter):
+	def __init__(self, fragment, depth=0, swappers=swaps, flaggers=flags, styles=styles, cstyles=cstyles):
+		Converter.__init__(self, fragment, depth, swappers, flaggers, styles, cstyles)
 		self.header = Header()
-		self.uncomment()
-
-	def uncomment(self):
-		cs = "<!--"
-		ce = "-->"
-		while cs in self.fragment:
-			start = self.fragment.index(cs)
-			end = self.fragment.index(ce, start)
-			self.fragment = self.fragment[:start] + self.fragment[end + 3:]
 
 	def translate(self):
-		self.swaps()
+		self.swapem()
 		self.translation = trans(self.translation, "table", TABLE_FLAGS)
 		self.bottomsup()
 		self.translation = self.header(self.translation, self.depth)
 		self.cleanup()
 		return self.translation
-
-	def swaps(self):
-		h = self.fragment
-		for swap in swaps:
-			h = h.replace(swap, swaps[swap])
-		self.translation = h
-
-	def bottomsup(self):
-		h = self.translation
-		flag = nextlast(h, flags)
-		while flag:
-			h = trans(h, flag)
-			flag = nextlast(h, flags)
-		self.translation = h
 
 	def cleanup(self):
 		self.translation = self.translation.replace("{#}", "{\\#}")
