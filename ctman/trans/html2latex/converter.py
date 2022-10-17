476e9790be5409368791a7153c9037c42ac5065e
bubbleboy14 <mario.balibrera@gmail.com>
2022-10-17 15:21:25 -0700

    trans.html2latex.converter: pswaps{} and iswaps{}; H2L constructor passes them (along with notswaps{}) to Converter constructor; rmed cleanup() and ifswaps() (now handled by Constructor base class)

--- ctman/trans/html2latex/converter.py
+++ ctman/trans/html2latex/converter.py
@@ -2,15 +2,19 @@ from .util import TABLE_FLAGS, flags, swaps, styles, cstyles, trans, Converter
 from .header import Header
 
 linestrips = ["NEWPAGE"]
-ifswaps = {
+pswaps = {
+	"{#}": "{\\#}",
+	"NEWPAGE": "\\newpage"
+}
+iswaps = {
 	"\\begin{center}": {
 		" \\hfill\\break ": " \\\\ "
 	}
 }
 
 class H2L(Converter):
-	def __init__(self, fragment, depth=0, swappers=swaps, flaggers=flags, styles=styles, cstyles=cstyles, linestrips=linestrips, loud=False):
-		Converter.__init__(self, fragment, depth, swappers, flaggers, styles, cstyles, linestrips, loud)
+	def __init__(self, fragment, depth=0, swappers=swaps, flaggers=flags, styles=styles, cstyles=cstyles, linestrips=linestrips, postswaps=pswaps, ifswaps=iswaps, notswaps={}, loud=False):
+		Converter.__init__(self, fragment, depth, swappers, flaggers, styles, cstyles, linestrips, postswaps, ifswaps, notswaps, loud)
 		self.header = Header()
 
 	def translate(self):
@@ -20,18 +24,3 @@ class H2L(Converter):
 		self.translation = self.header(self.translation, self.depth)
 		self.cleanup()
 		return self.translation
-
-	def cleanup(self):
-		self.translation = self.translation.replace("{#}", "{\\#}")
-		self.translation = self.translation.replace("NEWPAGE", "\\newpage")
-		self.ifswaps()
-
-	def ifswaps(self):
-		lines = []
-		for line in self.translation.split("\n"):
-			for swap in ifswaps:
-				if swap in line:
-					for k, v in ifswaps[swap].items():
-						line = line.replace(k, v)
-			lines.append(line)
-		self.translation = "\n".join(lines)
