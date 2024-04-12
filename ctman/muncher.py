b7bf33de90a83be704691288363cf6b0b5b3f2a9
bubbleboy14 <mario.balibrera@gmail.com>
2024-04-11 23:55:44 -0700

    nearly completed Linker - linkify() (which is mostly correct), and finishing touches on linx() and fix()

--- ctman/muncher.py
+++ ctman/muncher.py
@@ -5,6 +5,7 @@ from model import Chemical
 PRUPROPS = ["classification", "code", "cas", "formula", "physical_description"]
 LINPROPS = ["rtecs", "idlh", "respirator_recommendations", "exposure_limits",
 	"measurement_methods", "first_aid", "personal_protection_sanitation"]
+NIOSHURL = "https://www.cdc.gov"
 
 class Pruner(object):
 	def __init__(self, items):
@@ -53,6 +54,13 @@ class Linker(object):
 	def log(self, msg):
 		log("Linker : %s"(msg,))
 
+	def linkify(self, orig):
+		if orig.startswith("http") or orig.startswith("//"):
+			return orig
+		if orig.startswith("/"):
+			return NIOSHURL + orig
+		return "%s/%s"%(NIOSHURL, orig) # NB : this probs isn't right!
+
 	def linx(self, text):
 		parts = text.split(" href")
 		newparts = []
@@ -62,25 +70,35 @@ class Linker(object):
 			doub = part.find('"')
 			if sing == -1:
 				quote = '"'
-				qindex = doub
+				qstart = doub
 			elif doub == -1:
 				quote = "'"
-				qindex = sing
+				qstart = sing
 			else:
 				smallsing = sing < doub
 				quote = smallsing and "'" or '"'
-				qindex = smallsing and sing or doub
-			link = part[qindex:part.find(quote, qindex + 1)]
-			# TODO : if link.startswith(...)
-
+				qstart = smallsing and sing or doub
+			qstart += 1
+			qend = part.find(quote, qstart)
+			link = part[qstart:qend]
+			newlink = self.linkify(link)
+			if link != newlink:
+				changed = True
+				part = part[:qstart] + newlink + part[qend:]
+			newparts.append(part)
+		return changed and " href".join(newparts)
 
 	def fix(self):
+		changed = set()
 		for item in self.items:
 			for prop in LINPROPS:
 				val = getattr(item, prop)
-				links = self.linx(val)
-
-
+				linkified = self.linx(val)
+				if linkified:
+					setattr(item, prop, linkified)
+					changed.add(item)
+		if changed and confirm("fix %s rows?"%(len(changed),)):
+			db.put_multi(list(changed))
 
 class Muncher(object):
 	def __init__(self, model=Chemical):
